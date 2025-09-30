fd8db00e44036903ad26de6b518412ec9a4ba147
mhandousa <53653174+mhandousa@users.noreply.github.com>
2025-09-30 10:08:57 +0300

    Align favicon links with available assets

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,25 +26,12 @@ export default function MyDocument({ locale }: Props) {
         />
         
         {/* Favicon and App Icons */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="48x48" href="/icons/favicon-48x48.png" />
-        
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/icon1.png" />
+        <link rel="icon" type="image/svg+xml" href="/icons/icon0.svg" />
+
         {/* Apple Touch Icons */}
-        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
-        
-        {/* Android Chrome Icons */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icons/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon.png" />
         
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
@@ -52,7 +39,7 @@ export default function MyDocument({ locale }: Props) {
         {/* Theme Colors */}
         <meta name="theme-color" content="#0ea5e9" />
         <meta name="msapplication-TileColor" content="#0ea5e9" />
-        <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png" />
+        <meta name="msapplication-TileImage" content="/icons/apple-icon.png" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         
         {/* iOS Specific */}
